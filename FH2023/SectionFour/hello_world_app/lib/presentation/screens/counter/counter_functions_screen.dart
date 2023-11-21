2e72dc4c2db11cd00d984c4fe740c1df874a10f8
Erkan Öztürk <erkanozturk1903@yahoo.com.tr>
2023-11-21 11:21:46 +0300

    V1

---
+++ FH2023/SectionFour/hello_world_app/lib/presentation/screens/counter/counter_functions_screen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class CounterFunctionsScreen extends StatefulWidget {
+  const CounterFunctionsScreen({super.key});
+
+  @override
+  State<CounterFunctionsScreen> createState() => _CounterFunctionsScreenState();
+}
+
+class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
+  int clickCounter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Counter Functions"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  clickCounter = 0;
+                });
+              },
+              icon: const Icon(Icons.refresh_rounded),
+            ),
+          ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "$clickCounter",
+                style: const TextStyle(
+                  fontSize: 160,
+                  fontWeight: FontWeight.w100,
+                ),
+              ),
+              Text(
+                "Click${clickCounter == 1 ? '' : 's'}",
+                style: const TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            CustomFloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  clickCounter = 0;
+                });
+              },
+              icon: Icons.refresh_rounded,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            CustomFloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  clickCounter++;
+                });
+              },
+              icon: Icons.plus_one,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            CustomFloatingActionButton(
+              onPressed: () {
+                if (clickCounter == 0) return;
+                clickCounter--;
+              },
+              icon: Icons.exposure_minus_1,
+            ),
+          ],
+        ));
+  }
+}
+
+class CustomFloatingActionButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback? onPressed;
+
+  const CustomFloatingActionButton({
+    super.key,
+    required this.icon,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      //shape: const StadiumBorder(),
+      enableFeedback: true,
+      elevation: 5,
+      //backgroundColor: Colors.yellow,
+      onPressed: onPressed,
+      child: Icon(icon),
+    );
+  }
+}
