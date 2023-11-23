21fa12817d2295af8dab1ef60f1f082d7df8b930
Erkan Öztürk <erkanozturk1903@yahoo.com.tr>
2023-11-23 08:59:52 +0300

    V4

---
+++ FH2023/SectionSeven/toktik_app/android/app/src/main/kotlin/com/erkanozturk1903/toktik_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.erkanozturk1903.toktik_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ FH2023/SectionSeven/toktik_app/lib/shared/data/local_video_post.dart
@@ -0,0 +1,50 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Yürüyen merdivenlerden çıkma',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': 23230,
+    'views': 1523,
+  },
+  {
+    'name': 'Yayalar tarafından beğenilen çiçek',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': 24230,
+    'views': 1343,
+  },
+  {
+    'name': 'Herşeyi ne kardar bulanık görüyorum',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': 21564320,
+    'views': 123563,
+  },
+  {
+    'name': 'Buğday mı? Ne kadar ilginç',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': 320,
+    'views': 2300,
+  },
+  {
+    'name': 'COVID beni etkilemiyor',
+    'videoUrl': 'assets/videos/5.mp4',
+    'likes': 3230,
+    'views': 31030,
+  },
+  {
+    'name': 'Bugün işe gitmek istemiyorum By Stark',
+    'videoUrl': 'assets/videos/6.mp4',
+    'likes': 10,
+    'views': 330,
+  },
+  {
+    'name': 'Temizlik hiç bu kadar eğlenceli olmamıştı',
+    'videoUrl': 'assets/videos/7.mp4',
+    'likes': 1320,
+    'views': 33032,
+  },
+  {
+    'name': 'Türkiye ye henüz gelmedik...hımm',
+    'videoUrl': 'assets/videos/8.mp4',
+    'likes': 342,
+    'views': 3332,
+  },
+];
