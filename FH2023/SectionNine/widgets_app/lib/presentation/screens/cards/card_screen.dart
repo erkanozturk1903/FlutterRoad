fd0bf95186daf32002563a042503c5f9ab1328fd
Erkan Öztürk <erkanozturk1903@yahoo.com.tr>
2023-11-24 11:37:59 +0300

    V7

---
+++ FH2023/SectionNine/widgets_app/lib/presentation/screens/cards/card_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CardScreen extends StatelessWidget {
+  static const String name = 'cards_screen';
+  const CardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Card Screen"),
+      ),
+      body: Center(
+        child: Text("Card Screen"),
+      ),
+    );
+  }
+}
