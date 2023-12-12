1e923c8dab28f5765ccd8007b19bd37c969128b3
Erkan Öztürk <erkanozturk1903@yahoo.com.tr>
2023-12-12 16:35:18 +0300

    V24

--- FH2023/Cinemapedia/cinemapedia/lib/infrastructure/datasources/moviedb_datasource.dart
+++ FH2023/Cinemapedia/cinemapedia/lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,9 +1,11 @@
 import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/domain/datasource/movie_datasource.dart';
 import 'package:cinemapedia/domain/entitiy/movie.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
-class MoviedbDatasource extends MovieDatasource {
+class MoviedbDatasource extends MoviesDatasource {
   final dio = Dio(
     BaseOptions(
       baseUrl: 'https://api.themoviedb.org/3',
@@ -17,7 +19,13 @@ class MoviedbDatasource extends MovieDatasource {
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get('/movie/now_playing');
-    final List<Movie> movies = [];
+
+    final movieDBResponse = MovieDbResponse.fromMap(response.data);
+
+    final List<Movie> movies = movieDBResponse.results
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
+        .toList();
     return movies;
   }
 }
